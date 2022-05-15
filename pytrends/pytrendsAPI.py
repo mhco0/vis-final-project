bfed87c10ab22b20fe797201888d6c20945bd5aa
Thalisson <thalissonmt@gmail.com>
2022-05-14 21:36:09 -0300

    ajustando API para retornar search cem UTF-8

--- pytrends/pytrendsAPI.py
+++ pytrends/pytrendsAPI.py
@@ -2,18 +2,19 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 import pandas as pd                        
 from pytrends.request import TrendReq
+from urllib.parse import urlparse, unquote
 
 class requestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         global pytrend
         self.send_response(200)
         self.send_header('content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', 'http://localhost:8000')
         self.end_headers()
         print(self.path)
         method = self.extractMethod(self.path)
         if method == '/InterestOverTime':
             search = self.extractSearch(self.path)
-            search = search.replace('%20', ' ')
             pytrend.build_payload([search])
             df = pytrend.interest_over_time()
             print(df.head())
@@ -22,11 +23,10 @@ class requestHandler(BaseHTTPRequestHandler):
 
         elif method == '/InterestByRegion':
             search = self.extractSearch(self.path)
-            search = search.replace('%20', ' ')
             pytrend.build_payload([search])
             df = pytrend.interest_by_region(resolution='COUNTRY', inc_low_vol=True,  inc_geo_code=True)
             print(df.head())
-            js = df.to_json(orient = 'records')
+            js = df.to_json(orient = 'index')
             self.wfile.write(js.encode())
     
     def extractMethod(self, path):
@@ -34,12 +34,10 @@ class requestHandler(BaseHTTPRequestHandler):
         return method[0] if len(method) > 0 else ''
     
     def extractSearch(self, path):
-        splitedSearch = path.split('search=')[1]
-        splitedSearch = splitedSearch.split('-')
-        search = ''
-        for string in splitedSearch:
-            search = search + ' ' + string
-        return search
+        query = urlparse(path).query
+        query_components = dict(qc.split("=") for qc in query.split("&"))
+        search = query_components["search"]
+        return unquote(search)
 
 
 
